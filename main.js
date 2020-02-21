a441d9639f72eedd9eaf50bb72c969f39b7d0d39
Mark <markmdvd@gmail.com>
2020-02-20 21:11:55 -0800

    a

--- main.js
+++ main.js
@@ -21,13 +21,13 @@ var FSHADER_SOURCE =
     'varying vec4 v_Color;\n' +
     'varying vec2 v_TexCoord;\n' +
     'uniform sampler2D u_Sampler;\n' +
-    'uniform int u_Text;\n'+
-    'uniform vec4 u_Color;\n'+
+    'uniform int u_Text;\n' +
+    'uniform vec4 u_Color;\n' +
     'void main() {\n' +
-    'if ( u_Text == 1 ) {\n'+
+    'if ( u_Text == 1 ) {\n' +
     '  gl_FragColor = texture2D(u_Sampler, v_TexCoord);}\n' +
     'else{' +
-    '   gl_FragColor = u_Color;}\n'+
+    '   gl_FragColor = u_Color;}\n' +
     '   }\n';
 
 let gl;
@@ -37,13 +37,21 @@ let textNum;
 let u_Sampler;
 let u_Text;
 let u_Color;
+let eyeObj = {};
+let G_STEP = .05;
+
+let array = [[1, 0, 0, 1],
+    [1, 1, 0, 1],
+    [1, 0, 0, 1],
+    [1, 1, 1, 1]];
+
 function main() {
     textNum = 0;
     // Retrieve <canvas> element
     var canvas = document.getElementById('webgl');
 
     // Get the rendering context for WebGL
-     gl = getWebGLContext(canvas);
+    gl = getWebGLContext(canvas);
     if (!gl) {
         console.log('Failed to get the rendering context for WebGL');
         return;
@@ -73,14 +81,15 @@ function main() {
         return;
     }
 
-    u_Text = gl.getUniformLocation(gl.program,'u_Text');
-    gl.uniform1i(u_Text,1);
+    u_Text = gl.getUniformLocation(gl.program, 'u_Text');
+    gl.uniform1i(u_Text, 1);
 
-    u_Color = gl.getUniformLocation(gl.program,'u_Color');
+    u_Color = gl.getUniformLocation(gl.program, 'u_Color');
 
     mvpMatrix = new Matrix4();
     mvpMatrix.setPerspective(30, 1, 1, 100);
-    mvpMatrix.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
+    eyeObj = {"x": 10, "y": 2, "z": 10, "lookX": 0, "lookY": 2, "lookZ": 0};
+    mvpMatrix.lookAt(eyeObj.x, eyeObj.y, eyeObj.z, eyeObj.lookX, eyeObj.lookY, eyeObj.lookZ, 0, 1, 0);
 
     // Pass the model view projection matrix to u_MvpMatrix
     gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
@@ -95,18 +104,38 @@ function main() {
     // Draw the cube
 }
 
+function draw(n) {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    mvpMatrix = new Matrix4();
+    mvpMatrix.setPerspective(30, 1, 1, 100);
+    mvpMatrix.lookAt(eyeObj.x, eyeObj.y, eyeObj.z, eyeObj.lookX, eyeObj.lookY, eyeObj.lookZ, 0, 1, 0);
+    gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
 
-function drawCube(center,u_MvpMatrix,mvpMatrix,scale,n,color) {
-    gl.uniform1i(u_Text,color.type);
-    if (color.type === 0){
+    for (let len = 0; len < array.length; len++) {
+        for (let width = 0; width < array[len].length; width++) {
+            if (array[len][width] === 1) {
+                drawCube({"x": len, "y": 0, "z": width}, u_MvpMatrix, mvpMatrix, {
+                    "x": 1,
+                    "y": 1,
+                    "z": 1
+                }, n, {"type": 1, "texID": 2})
+            }
+        }
+    }
+}
+
+
+function drawCube(center, u_MvpMatrix, mvpMatrix, scale, n, color) {
+    gl.uniform1i(u_Text, color.type);
+    if (color.type === 0) {
         gl.uniform4fv(u_Color, new Float32Array([color.rgb.red, color.rgb.green, color.rgb.blue, 1.0]));
 
     } else if (color.type === 1) {
         gl.uniform1i(u_Sampler, color.texID);
     }
     let model = new Matrix4();
-    model.translate(center.x,center.y,center.z);
-    model.scale(scale.x,scale.y,scale.z);
+    model.translate(center.x, center.y, center.z);
+    model.scale(scale.x, scale.y, scale.z);
 
     let mvp = new Matrix4();
     mvp.set(mvpMatrix);
@@ -133,39 +162,39 @@ function initVertexBuffers(gl) {
     //  v1------v2
 
     var vertices = new Float32Array([   // Vertex coordinates
-        0, 0, 1,  1, 0, 1,  1, 1, 1,  0, 1, 1,     // Front face
-        0, 0, 0,  0, 1, 0,  1, 1, 0,  1, 0, 0,     // Back face
-        0, 1, 0,  0, 1, 1,  1, 1, 1,  1, 1, 0,     // Top face
-        0, 0, 0,  1, 0, 0,  1, 0, 1,  0, 0, 1,     // Bottom face
-        1, 0, 0,  1, 1, 0,  1, 1, 1,  1, 0, 1,     // Right face
-        0, 0, 0,  0, 0, 1,  0, 1, 1,  0, 1, 0,     // Left face
+        0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1,     // Front face
+        0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0,     // Back face
+        0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0,     // Top face
+        0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1,     // Bottom face
+        1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1,     // Right face
+        0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0,     // Left face
     ]);
 
     var colors = new Float32Array([     // Colors
-        0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  // v0-v1-v2-v3 front(blue)
-        0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  // v4-v7-v6-v5 back
-        1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  // v0-v5-v6-v1 up(red)
-        1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  // v7-v4-v3-v2 down
-        0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  // v0-v3-v4-v5 right(green)
-        1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  // v1-v6-v7-v2 left
+        0.4, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4, 0.4, 1.0,  // v0-v1-v2-v3 front(blue)
+        0.4, 1.0, 1.0, 0.4, 1.0, 1.0, 0.4, 1.0, 1.0, 0.4, 1.0, 1.0,  // v4-v7-v6-v5 back
+        1.0, 0.4, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4, 0.4,  // v0-v5-v6-v1 up(red)
+        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,  // v7-v4-v3-v2 down
+        0.4, 1.0, 0.4, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4,  // v0-v3-v4-v5 right(green)
+        1.0, 1.0, 0.4, 1.0, 1.0, 0.4, 1.0, 1.0, 0.4, 1.0, 1.0, 0.4,  // v1-v6-v7-v2 left
     ]);
 
     var texCoords = new Float32Array([
-        0.0, 0.0,  1.0, 0.0,  1.0, 1.0,  0.0, 1.0,   // Front
-        1.0, 0.0,  1.0, 1.0,  0.0, 1.0,  0.0, 0.0,   // Back
-        0.0, 0.0,  1.0, 0.0,  1.0, 1.0,  0.0, 1.0,   // Top
-        0.0, 0.0,  1.0, 0.0,  1.0, 1.0,  0.0, 1.0,   // Bottom
-        0.0, 0.0,  0.0, 1.0,  1.0, 1.0,  1.0, 0.0,   // Right
-        1.0, 0.0,  0.0, 0.0,  0.0, 1.0,  1.0, 1.0,   // Left
+        0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,   // Front
+        1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0,   // Back
+        0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,   // Top
+        0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,   // Bottom
+        0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0,   // Right
+        1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0,   // Left
     ]);
 
     var indices = new Uint8Array([       // Indices of the vertices
-        0, 1, 2,   0, 2, 3,    // front
-        4, 5, 6,   4, 6, 7,    // right
-        8, 9,10,   8,10,11,    // up
-        12,13,14,  12,14,15,    // left
-        16,17,18,  16,18,19,    // down
-        20,21,22,  20,22,23     // back
+        0, 1, 2, 0, 2, 3,    // front
+        4, 5, 6, 4, 6, 7,    // right
+        8, 9, 10, 8, 10, 11,    // up
+        12, 13, 14, 12, 14, 15,    // left
+        16, 17, 18, 16, 18, 19,    // down
+        20, 21, 22, 20, 22, 23     // back
     ]);
 
     // Create a buffer object
@@ -180,7 +209,7 @@ function initVertexBuffers(gl) {
     if (!initArrayBuffer(gl, colors, 3, gl.FLOAT, 'a_Color'))
         return -1;
 
-    if (!initArrayBuffer(gl,texCoords,2,gl.FLOAT,'a_TexCoord'))
+    if (!initArrayBuffer(gl, texCoords, 2, gl.FLOAT, 'a_TexCoord'))
         return -1;
     // Write the indices to the buffer object
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
@@ -213,18 +242,18 @@ function initArrayBuffer(gl, data, num, type, attribute) {
 
 
 function initTextures(gl, n) {
-      // Clear <canvas>
+    // Clear <canvas>
 
-    let images = ['resources/wall.jpg','resources/sky.jpg'];
-    let texArr = [gl.TEXTURE0,gl.TEXTURE1,gl.TEXTURE2,gl.TEXTURE3,gl.TEXTURE4,gl.TEXTURE5,gl.TEXTURE6,gl.TEXTURE7];
+    let images = ['resources/wall.jpg', 'resources/sky.jpg', 'resources/stone.jpg'];
+    let texArr = [gl.TEXTURE0, gl.TEXTURE1, gl.TEXTURE2, gl.TEXTURE3, gl.TEXTURE4, gl.TEXTURE5, gl.TEXTURE6, gl.TEXTURE7];
 
-     u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
+    u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
     if (!u_Sampler) {
         console.log('Failed to get the storage location of u_Sampler');
         return false;
     }
 
-    images.forEach(function(source, idx, imgArray){
+    images.forEach(function (source, idx, imgArray) {
         var texture = gl.createTexture();   // Create a texture object
         if (!texture) {
             console.log('Failed to create the texture object');
@@ -237,14 +266,16 @@ function initTextures(gl, n) {
             return false;
         }
         // Register the event handler to be called on loading an image
-        image.onload = function(){ loadTexture(gl, n, texture, u_Sampler, image,texArr[idx]); };
+        image.onload = function () {
+            loadTexture(gl, n, texture, u_Sampler, image, texArr[idx]);
+        };
         // Tell the browser to load an image
         image.src = source;
     });
     return true;
 }
 
-function loadTexture(gl, n, texture, u_Sampler, image,texID) {
+function loadTexture(gl, n, texture, u_Sampler, image, texID) {
     textNum++;
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
     // Enable texture unit0
@@ -260,21 +291,109 @@ function loadTexture(gl, n, texture, u_Sampler, image,texID) {
     // Set the texture unit 0 to the sampler
 
 
-
     // gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
-    if (textNum === 2) {
-        gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
-        drawCube(
-            {"x": 0, "y": 0, "z": 1},
-            u_MvpMatrix,
-            mvpMatrix,
-            {"x": 1, "y": 1, "z": 1},
-                  n,
-            {"type":0,"rgb":{"red":0.5,"green":0.5,"blue":0.5}});
-        drawCube({"x": 1, "y": 0, "z": 0}, u_MvpMatrix, mvpMatrix,
-            {"x": 1, "y": 1, "z": 1},
-            n,
-            {"type":1,"texID":1});
+    if (textNum === 3) {
+        document.onkeydown = function (ev) {
+            keydown(ev, gl, n,);
+        };
+        draw(n);
+        // gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+        // drawCube(
+        //     {"x": 0, "y": 0, "z": 1},
+        //     u_MvpMatrix,
+        //     mvpMatrix,
+        //     {"x": 1, "y": 1, "z": 1},
+        //           n,
+        //     {"type":0,"rgb":{"red":0.5,"green":0.5,"blue":0.5}});
+        // drawCube({"x": 1, "y": 0, "z": 0}, u_MvpMatrix, mvpMatrix,
+        //     {"x": 1, "y": 1, "z": 1},
+        //     n,
+        //     {"type":1,"texID":1});
     }
 }
+
+function keydown(ev, gl, n) {
+    switch (ev.which) {
+        case 87: // W
+            console.log("KeyW");
+            moveForward();
+            break;
+        case 83: // S
+            console.log("KeyS");
+            moveBackward();
+            break;
+        case 65: // A
+            console.log("KeyA");
+            moveLeft();
+            break;
+        case 68: // D
+            console.log("KeyD");
+            moveRight();
+            break;
+    }
+    draw(n);
+}
+
+function moveForward() {
+
+    let at = new Vector3([eyeObj.lookX, eyeObj.lookY, eyeObj.lookZ]);
+    let eye = new Vector3([eyeObj.x, eyeObj.y, eyeObj.z]);
+    let dir = at.sub(eye);
+    let sdir = dir.mul(G_STEP);
+    let nEye = eye.add(sdir);
+    let nAt = at.add(sdir);
+    eyeObj.x = nEye.elements[0];
+// eyeObj.y = nEye.y;
+    eyeObj.z = nEye.elements[2];
+
+    eyeObj.lookX = nAt.elements[0];
+    eyeObj.lookY = nAt.elements[1];
+    eyeObj.lookZ = nAt.elements[2];
+    console.log(eyeObj);
+
+}
+
+function moveBackward() {
+    let at = new Vector3([eyeObj.lookX, eyeObj.lookY, eyeObj.lookZ]);
+    let eye = new Vector3([eyeObj.x, eyeObj.y, eyeObj.z]);
+    let dir = at.sub(eye);
+    let sdir = dir.mul(G_STEP);
+    let nEye = eye.sub(sdir);
+    let nAt = at.sub(sdir);
+    eyeObj.x = nEye.elements[0];
+// eyeObj.y = nEye.y;
+    eyeObj.z = nEye.elements[2];
+
+    eyeObj.lookX = nAt.elements[0];
+    eyeObj.lookY = nAt.elements[1];
+    eyeObj.lookZ = nAt.elements[2];
+    console.log(eyeObj);
+}
+
+function getDeltasLR() {
+    let dirX = eyeObj.x - eyeObj.lookX;
+    let dirZ = eyeObj.z - eyeObj.lookZ;
+    let dirH = Math.sqrt(dirX * dirX + dirZ * dirZ);
+    let deltaX = G_STEP * dirZ / dirH;
+    let deltaZ = G_STEP * dirX / dirH;
+    return {deltaX, deltaZ};
+}
+
+
+function moveRight() {
+    let { deltaX, deltaZ } = getDeltasLR();
+    eyeObj.x = eyeObj.x + deltaX;
+    eyeObj.z = eyeObj.z - deltaZ;
+    eyeObj.lookX = eyeObj.lookX + deltaX;
+    eyeObj.lookZ = eyeObj.lookZ - deltaZ;
+}
+
+function moveLeft() {
+    let { deltaX, deltaZ } = getDeltasLR();
+    eyeObj.x = eyeObj.x - deltaX;
+    eyeObj.z = eyeObj.z + deltaZ;
+    eyeObj.lookX = eyeObj.lookX - deltaX;
+    eyeObj.lookZ = eyeObj.lookZ + deltaZ;
+}
+
 
